7ac796d699760a9caf2ee8ca70f882858eb6da33
Devsethi3 <devsethi4522@gmail.com>
2025-07-13 21:16:58 +0530

    feat: render course description from TipTap editor

---
+++ src/components/rich-text-editor/RenderDescription.tsx
@@ -0,0 +1,13 @@
+import { useMemo } from 'react';
+import { generateHTML} from '@tiptap/react'
+import {type JSONContent} from '@tiptap/react'
+
+const RenderDescription = ({json}:{json:JSONContent}) => {
+    const outPut = useMemo(() => {
+    })
+  return (
+    <div>RenderDescription</div>
+  )
+}
+
+export default RenderDescription
