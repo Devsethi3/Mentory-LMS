bf5142dd62c9b97d0cedadc5f493df5153425797
Devsethi3 <devsethi4522@gmail.com>
2025-07-14 13:54:22 +0530

    feat(course): complete course slug page with dynamic content, collapsible chapters, and enrollment card

--- src/app/(main)/courses/[slug]/page.tsx
+++ src/app/(main)/courses/[slug]/page.tsx
@@ -4,15 +4,23 @@ import Image from "next/image";
 import Header from "../../_components/Header";
 import { Badge } from "@/components/ui/badge";
 import {
+  IconBook,
   IconCategory,
   IconChartBar,
   IconChevronDown,
   IconClock,
+  IconPlayerPlay,
 } from "@tabler/icons-react";
 import { Separator } from "@/components/ui/separator";
 import RenderDescription from "@/components/rich-text-editor/RenderDescription";
-import { Collapsible, CollapsibleTrigger } from "@/components/ui/collapsible";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { Card, CardContent } from "@/components/ui/card";
+import { CheckIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type Params = Promise<{ slug: string }>;
 
@@ -88,7 +96,7 @@ const SlugPage = async ({ params }: { params: Params }) => {
             <div className="space-y-1">
               {course.chapter.map((chapter, index) => (
                 <Collapsible key={chapter.id} defaultOpen={index === 0}>
-                  <Card className="py-0 overflow-hidden border-2 transition-all duration-200 hover:shadow-md">
+                  <Card className="py-0 overflow-hidden border-2 transition-all duration-200 hover:shadow-md gap-0">
                     <CollapsibleTrigger>
                       <div>
                         <CardContent className="p-6 hover:bg-muted/50">
@@ -118,10 +126,141 @@ const SlugPage = async ({ params }: { params: Params }) => {
                         </CardContent>
                       </div>
                     </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <div className="border-t bg-muted/20">
+                        <div className="p-5 pt-4 space-y-3">
+                          {chapter.lessons.map((lesson, lessonIndex) => (
+                            <div
+                              key={lesson.id}
+                              className="flex items-center gap-4 rounded-lg p-3 hover:bg-accent group"
+                            >
+                              <div className="flex size-8 items-center justify-center rounded-full bg-background border-2 border-primary/20">
+                                <IconPlayerPlay className="size-4 text-muted-foreground group-hover:text-primary" />
+                              </div>
+
+                              <div className="flex-1">
+                                <p className="font-medium text-sm">
+                                  {lesson.title}
+                                </p>
+                                <p className="text-xs text-muted-foreground mt-1">
+                                  Lesson {lessonIndex + 1}
+                                </p>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </CollapsibleContent>
                   </Card>
                 </Collapsible>
               ))}
             </div>
+          </div>
+        </div>
+
+        {/* Enrollment Card */}
+
+        <div className="order-2 lg:col-span-1">
+          <div className="sticky top-20">
+            <Card className="py-0 gap-0">
+              <CardContent className="p-6">
+                <div className="flex items-center justify-between mb-6">
+                  <span className="text-lg font-medium">Price</span>
+                  <span className="text-2xl font-bold text-primary">
+                    {new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    }).format(course.price)}
+                  </span>
+                </div>
+                <div className="space-y-4 mb-6 rounded-lg bg-muted p-4">
+                  <h4 className="font-medium">What you will get:</h4>
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center gap-3">
+                      <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                        <IconClock className="size-4" />
+                      </div>
+                      <div>
+                        <p className="text-sm font-medium">Course Duration</p>
+                        <p className="text-sm text-muted-foreground">
+                          {course.duration} minutes
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center gap-3">
+                      <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                        <IconChartBar className="size-4" />
+                      </div>
+                      <div>
+                        <p className="text-sm font-medium">Difficulty Level </p>
+                        <p className="text-sm text-muted-foreground">
+                          {course.level}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center gap-3">
+                      <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                        <IconCategory className="size-4" />
+                      </div>
+                      <div>
+                        <p className="text-sm font-medium">Category</p>
+                        <p className="text-sm text-muted-foreground">
+                          {course.category}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center gap-3">
+                      <div className="flex size-8 items-center justify-center rounded-full bg-primary/10 text-primary">
+                        <IconBook className="size-4" />
+                      </div>
+                      <div>
+                        <p className="text-sm font-medium">Total Lessons</p>
+                        <p className="text-sm text-muted-foreground">
+                          {course.chapter.reduce(
+                            (total, chapter) => total + chapter.lessons.length,
+                            0
+                          ) || 0}{" "}
+                          Lessons
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-6 space-y-6">
+                  <h4>This course includes:</h4>
+                  <ul className="space-y-2">
+                    <li className="flex items-center gap-2 text-sm">
+                      <div className="rounded-full p-1 bg-green-500/10 text-green-500">
+                        <CheckIcon className="size-3" />
+                      </div>
+                      <span>Full lifetime access</span>
+                    </li>
+                    <li className="flex items-center gap-2 text-sm">
+                      <div className="rounded-full p-1 bg-green-500/10 text-green-500">
+                        <CheckIcon className="size-3" />
+                      </div>
+                      <span>Access on mobile and desktop</span>
+                    </li>
+                    <li className="flex items-center gap-2 text-sm">
+                      <div className="rounded-full p-1 bg-green-500/10 text-green-500">
+                        <CheckIcon className="size-3" />
+                      </div>
+                      <span>Certificate of completion</span>
+                    </li>
+                  </ul>
+                </div>
+                <Button className="w-full">Enroll Now!</Button>
+                <p className="mt-3 text-center text-xs text-muted-foreground">
+                  30-day money-back guarantee
+                </p>
+              </CardContent>
+            </Card>
           </div>
         </div>
       </div>
@@ -130,5 +269,3 @@ const SlugPage = async ({ params }: { params: Params }) => {
 };
 
 export default SlugPage;
-
-// git commit message for making this page rendering lessons and chapters just share the git message nothing else
