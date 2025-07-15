582b8b5804706d7dbf8ddcebf82a3101e9044281
Devsethi3 <devsethi4522@gmail.com>
2025-07-15 22:09:47 +0530

    feat(admin): integrate real analytics data into dashboard graphs

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,59 @@
 import { ChartAreaInteractive } from "@/components/sidebar/chart-area-interactive";
 import { SectionCards } from "@/components/sidebar/section-cards";
+import { buttonVariants } from "@/components/ui/button";
+import { adminGetEnrollmentStats } from "@/data/admin/admin-get-enrollment-stats";
+import { adminGetRecentCourses } from "@/data/admin/admin-get-recent-courses";
+import Link from "next/link";
+import EmptyState from "@/components/general/EmptyState";
+import AdminCourseCard from "./courses/_components/AdminCourseCard";
+import { Suspense } from "react";
 
-const AdminIndexPage = () => {
+const AdminIndexPage = async () => {
+  const enrollmentData = await adminGetEnrollmentStats();
   return (
     <>
       <SectionCards />
-      <ChartAreaInteractive />
+      <ChartAreaInteractive data={enrollmentData} />
+
+      <div className="space-y-4">
+        <div className="flex items-center justify-baseline">
+          <h2 className="text-xl font-semibold">Recent Courses</h2>{" "}
+          <Link
+            href="/admin/courses"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            View All Courses
+          </Link>
+        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+          <RenderRecentCourses />
+        </Suspense>
+      </div>
     </>
   );
 };
 
 export default AdminIndexPage;
+
+async function RenderRecentCourses() {
+  const data = await adminGetRecentCourses();
+
+  if (data.length === 0) {
+    return (
+      <EmptyState
+        buttonText="Create New Course"
+        description="You don't have any courses. Create some courses to see them here"
+        title="You don't have any courses yet!"
+        href="/admin/courses/create"
+      />
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      {data.map((course) => (
+        <AdminCourseCard key={course.id} data={course} />
+      ))}
+    </div>
+  );
+}
--- src/components/sidebar/chart-area-interactive.tsx
+++ src/components/sidebar/chart-area-interactive.tsx
@@ -18,40 +18,6 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-export const description = "An interactive area chart";
-
-const dummyData = [
-  { date: "2024-06-01", enrollments: 12 },
-  { date: "2024-06-02", enrollments: 23 },
-  { date: "2024-06-03", enrollments: 17 },
-  { date: "2024-06-04", enrollments: 35 },
-  { date: "2024-06-05", enrollments: 28 },
-  { date: "2024-06-06", enrollments: 14 },
-  { date: "2024-06-07", enrollments: 40 },
-  { date: "2024-06-08", enrollments: 19 },
-  { date: "2024-06-09", enrollments: 22 },
-  { date: "2024-06-10", enrollments: 33 },
-  { date: "2024-06-11", enrollments: 26 },
-  { date: "2024-06-12", enrollments: 31 },
-  { date: "2024-06-13", enrollments: 11 },
-  { date: "2024-06-14", enrollments: 29 },
-  { date: "2024-06-15", enrollments: 45 },
-  { date: "2024-06-16", enrollments: 18 },
-  { date: "2024-06-17", enrollments: 25 },
-  { date: "2024-06-18", enrollments: 37 },
-  { date: "2024-06-19", enrollments: 16 },
-  { date: "2024-06-20", enrollments: 30 },
-  { date: "2024-06-21", enrollments: 39 },
-  { date: "2024-06-22", enrollments: 21 },
-  { date: "2024-06-23", enrollments: 34 },
-  { date: "2024-06-24", enrollments: 27 },
-  { date: "2024-06-25", enrollments: 42 },
-  { date: "2024-06-26", enrollments: 24 },
-  { date: "2024-06-27", enrollments: 38 },
-  { date: "2024-06-28", enrollments: 32 },
-  { date: "2024-06-29", enrollments: 20 },
-  { date: "2024-06-30", enrollments: 21 },
-];
 
 const chartConfig = {
   enrollments: {
@@ -60,15 +26,15 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export function ChartAreaInteractive() {
-  const isMobile = useIsMobile();
-  const [timeRange, setTimeRange] = React.useState("90d");
+interface ChartAreaInteractiveProps {
+  data: { date: string; enrollments: number }[];
+}
 
-  React.useEffect(() => {
-    if (isMobile) {
-      setTimeRange("7d");
-    }
-  }, [isMobile]);
+export function ChartAreaInteractive({ data }: ChartAreaInteractiveProps) {
+  const totalEnrollmentsNumber = React.useMemo(
+    () => data.reduce((acc, curr) => acc + curr.enrollments, 0),
+    [data]
+  );
 
   return (
     <Card className="@container/card">
@@ -76,9 +42,11 @@ export function ChartAreaInteractive() {
         <CardTitle>Total Enrollments</CardTitle>
         <CardDescription>
           <span className="hidden @[540px]/card:block">
-            Total Enrollments for the last 30 days: 1200
+            Total Enrollments for the last 30 days: {totalEnrollmentsNumber}
           </span>
-          <span className="@[540px]/card:hidden">Last 30 days: 1200</span>
+          <span className="@[540px]/card:hidden">
+            Last 30 days: {totalEnrollmentsNumber}
+          </span>
         </CardDescription>
       </CardHeader>
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
@@ -87,7 +55,7 @@ export function ChartAreaInteractive() {
           className="aspect-auto h-[250px] w-full"
         >
           <BarChart
-            data={dummyData}
+            data={data}
             margin={{
               left: 12,
               right: 12,
